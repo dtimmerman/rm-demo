9b859b9192968318c1fd972c7a762af62a82f345
Derek Timmerman <timmermanderek@gmail.com>
2014-10-17 06:08:03 -0400

    Primitive POST /messages endpoint, which responds and delivers to mailgun api

--- README.md
+++ README.md
@@ -12,6 +12,40 @@ todo
 
 # API
 
-Enqueue a message for immediate delivery: POST http://localhost/v1/message/
+Response objects follow JSend standard: http://labs.omniti.com/labs/jsend
 
-Retrieve a sent message: GET http://localhost/v1/message/<messageid>
+## Methods
+
+### Create a template for a message to use
+
+POST http://localhost/v1/template
+
+Params:
+* [body]
+
+### Enqueue a message for delivery
+
+POST http://localhost/v1/message/
+
+Params:
+* [from]
+* [to]
+* [subject]
+* [body]
+
+### Retrieve a sent message
+
+GET http://localhost/v1/message/<messageid>
+
+# TODO
+
+- validator.js
+- exception handling
+- data store
+- mailgun callback error handling
+- mailgun callback save sent message id to data store
+- unit test
+- session store
+- email form html
+- form response endpoint
+- admin ui
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
-var request = require('request');
+var app = express();
+
+var bodyParser = require('body-parser');
+var urlencoded = bodyParser.urlencoded({
+  extended: false
+});
+
 var config = require('nconf');
 
 config.argv().env().file({
@@ -11,13 +17,64 @@ var mailgun = require('mailgun-js')({
   domain: config.get('mailgun:domain')
 });
 
-var data = {
-  from: 'Derek <timmermanderek@gmail.com>',
-  to: 'timmermanderek@gmail.com',
-  subject: 'Hello',
-  text: 'Testing mailgun'
-};
+app.disable('x-powered-by');
+app.listen(3000);
 
-mailgun.messages().send(data, function (error, body) {
-  console.log(body);
+app.post('/v1/message', function(req, res) {
+
+  urlencoded(req, res, function() {
+
+    var from = typeof req.body.from !== 'undefined' ? req.body.from : false;
+    var to = typeof req.body.from !== 'undefined' ? req.body.to : false;
+    var subject = typeof req.body.subject !== 'undefined' ? req.body.subject : '';
+    var body = typeof req.body.body !== 'undefined' ? req.body.body : false;
+
+    var resContent = {
+      status: null,
+      data: null
+    };
+
+    var mailgunOpts;
+
+    // invalid request
+    if (!from || !to || !body) {
+      res.writeHead(400);
+      resContent.status = 'fail';
+      resContent.data = {};
+      if (!from) {
+        resContent.data.from = 'Missing "from" parameter.';
+      }
+      if (!to) {
+        resContent.data.to = 'Missing "to" parameter.';
+      }
+      if (!body) {
+        resContent.data.body = 'Missing "body" parameter.';
+      }
+      res.write(JSON.stringify(resContent));
+      res.end();
+
+      return;
+
+    }
+
+    // valid request
+    mailgunOpts = {
+      from: from,
+      to: to,
+      subject: subject,
+      text: body
+    };
+
+    mailgun.messages().send(mailgunOpts, function(error, body) {
+      res.writeHead(200);
+      resContent.status = 'success';
+      resContent.data = {};
+      resContent.data.mailgun = body;
+      resContent.data.messageId = '000';
+      res.write(JSON.stringify(resContent));
+      res.end();
+    });
+
+  });
+
 });
