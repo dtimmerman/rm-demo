d598481d283f85501241400ed2227e86bad84eb6
Derek Timmerman <timmermanderek@gmail.com>
2014-10-19 20:13:48 -0400

    Chugging through ember tutorial...

--- public/js/router.js
+++ public/js/router.js
@@ -1,3 +1,9 @@
 Rebelmail.Router.map(function() {
   this.resource('forms', { path: '/admin' });
 });
+
+Rebelmail.FormsRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('form');
+  }
+});
